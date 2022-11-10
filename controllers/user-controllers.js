97ff58e8a1beb0259960d8aab378323359355f2b
DJMIKE20 <m.zimm20@gmail.com>
2022-11-10 09:52:25 -0500

    added delete routes and put routes for user

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -53,5 +53,34 @@ const userController = {
         User.create(body)
         .then(userData => res.json(userData))
         .catch(err => res.status(400).json(err));  
+    },
+
+    // PUT, update user by ID 
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate( 
+            { _id: params.id }, body, { new: true, runValidators: true }
+        )
+            .then(updatedUserData => {
+                if(!updatedUserData) {
+                    res.json({ message: 'No user can be found by this id!' })
+                } else {
+                    res.json(updatedUserData)
+                }
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+
+    //DELETE, remove a user from the db
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then(deleteUserData => {
+            if(!deleteUserData) {
+                res.json({ message: 'No user can be found by this id!' })
+            } else {
+                res.json(deleteUserData)
+            }
+        })
+        .catch(err => res.status(400).json(err));
     }
 }
